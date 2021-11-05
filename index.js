89ce375bd505909d62f009f0d5d4b2b8200ac22b
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-05 11:47:40 +1030

    populating README file via writeFile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+// const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of your project?", "what is your GitHub username?", "What is your email address?", "Please enter a brief description of your project", "Please enter the installation instructions for this project", "Please enter usage instructions for this project", "Please enter the contribution guidelines", "Please enter the test instructions for this project", "Please choose a licence for your application from the list of options"];
@@ -25,41 +25,55 @@ inquirer
         message: questions[2],
         name: names[2],
     },
-    {
-        type: 'input', 
-        message: questions[3],
-        name: names[3],
-    },
-    {
-        type: 'input', 
-        message: questions[4],
-        name: names[4],
-    },
-    {
-        type: 'input', 
-        message: questions[5],
-        name: names[5],
-    },
-    {
-        type: 'input', 
-        message: questions[6],
-        name: names[6],
-    },
-    {
-        type: 'input', 
-        message: questions[7],
-        name: names[7],
-    },
-    {
-        type: 'list', 
-        message: questions[8],
-        name: names[8],
-        choices: ["no license", "The Unlicense", "MIT license", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0"]
-    },
-    ])
+    // {
+    //     type: 'input', 
+    //     message: questions[3],
+    //     name: names[3],
+    // },
+    // {
+    //     type: 'input', 
+    //     message: questions[4],
+    //     name: names[4],
+    // },
+    // {
+    //     type: 'input', 
+    //     message: questions[5],
+    //     name: names[5],
+    // },
+    // {
+    //     type: 'input', 
+    //     message: questions[6],
+    //     name: names[6],
+    // },
+    // {
+    //     type: 'input', 
+    //     message: questions[7],
+    //     name: names[7],
+    // },
+    // {
+    //     type: 'list', 
+    //     message: questions[8],
+    //     name: names[8],
+    //     choices: ["no license", "The Unlicense", "MIT license", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0"]
+    // },
+    ]).then((data) => {
+        console.log(data);
+        
+        fs.writeFile('README.md', JSON.stringify(data), (err) => {
+        if (err) {console.log(err);} else {
+        console.log("the file has been written");}
+    })});
+    // }).then(data) {
+    //     fs.writeFile('README.md', data, (err) =>
+    //     if (err) console.log(err);
+    //     console.log("the file has been written");});
+    // }
+    
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+
+// }
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,84 +4,6 @@
 "no license", "The Unlicense", "MIT license", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0"
 
 function renderLicenseBadge(license) { }
-if (license.choice == "no lience") {
-  ""
-} else if (license.choice == "MIT license") {
-  `MIT License
-
-   Copyright (c) 2021 ${github}
-   
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-   
-   The above copyright notice and this permission notice shall be included in all
-   copies or substantial portions of the Software.
-   
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   SOFTWARE.`
-} else if (license.choice == "The Unlicense") {
-  `This is free and unencumbered software released into the public domain.
-
-   Anyone is free to copy, modify, publish, use, compile, sell, or
-   distribute this software, either in source code form or as a compiled
-   binary, for any purpose, commercial or non-commercial, and by any
-   means.
-   
-   In jurisdictions that recognize copyright laws, the author or authors
-   of this software dedicate any and all copyright interest in the
-   software to the public domain. We make this dedication for the benefit
-   of the public at large and to the detriment of our heirs and
-   successors. We intend this dedication to be an overt act of
-   relinquishment in perpetuity of all present and future rights to this
-   software under copyright law.
-   
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-   OTHER DEALINGS IN THE SOFTWARE.
-   
-   For more information, please refer to <https://unlicense.org>`
-} else if (license.choice == "GNU AGPLv3") {
-  `GNU AFFERO GENERAL PUBLIC LICENSE
-   Version 3, 19 November 2007
-
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Everyone is permitted to copy and distribute verbatim copies
-  of this license document, but changing it is not allowed.`
-} else if (license.choice == "GNU GPLv3") {
-  `GNU GENERAL PUBLIC LICENSE
-  Version 3, 29 June 2007
-
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Everyone is permitted to copy and distribute verbatim copies
-  of this license document, but changing it is not allowed.`
-} else if (license.choice == "GNU LGPLv3") {  
-  `GNU LESSER GENERAL PUBLIC LICENSE
-  Version 3, 29 June 2007
-
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Everyone is permitted to copy and distribute verbatim copies
-  of this license document, but changing it is not allowed.`
-} else if (license.choice == "Mozilla Public License 2.0") {  
-  `Mozilla Public License Version 2.0
-  ==================================`
-} else if (license.choice == "Apache License 2.0") {
-  `Apache License
-  Version 2.0, January 2004
-  http://www.apache.org/licenses/`
-}
 
   // TODO: Create a function that returns the license link
   // If there is no license, return an empty string
@@ -89,13 +11,92 @@ if (license.choice == "no lience") {
 
   // TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
-  function renderLicenseSection(license) { }
+  function renderLicenseSection(license) {
+    if (license.choice == "no lience") {
+      ""
+    } else if (license.choice == "MIT license") {
+      `MIT License
+    
+       Copyright (c) 2021 ${github}
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.`
+    } else if (license.choice == "The Unlicense") {
+      `This is free and unencumbered software released into the public domain.
+    
+       Anyone is free to copy, modify, publish, use, compile, sell, or
+       distribute this software, either in source code form or as a compiled
+       binary, for any purpose, commercial or non-commercial, and by any
+       means.
+       
+       In jurisdictions that recognize copyright laws, the author or authors
+       of this software dedicate any and all copyright interest in the
+       software to the public domain. We make this dedication for the benefit
+       of the public at large and to the detriment of our heirs and
+       successors. We intend this dedication to be an overt act of
+       relinquishment in perpetuity of all present and future rights to this
+       software under copyright law.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+       EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+       MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+       IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+       OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+       ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+       OTHER DEALINGS IN THE SOFTWARE.
+       
+       For more information, please refer to <https://unlicense.org>`
+    } else if (license.choice == "GNU AGPLv3") {
+      `GNU AFFERO GENERAL PUBLIC LICENSE
+       Version 3, 19 November 2007
+    
+      Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.`
+    } else if (license.choice == "GNU GPLv3") {
+      `GNU GENERAL PUBLIC LICENSE
+      Version 3, 29 June 2007
+    
+      Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.`
+    } else if (license.choice == "GNU LGPLv3") {  
+      `GNU LESSER GENERAL PUBLIC LICENSE
+      Version 3, 29 June 2007
+    
+      Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.`
+    } else if (license.choice == "Mozilla Public License 2.0") {  
+      `Mozilla Public License Version 2.0
+      ==================================`
+    } else if (license.choice == "Apache License 2.0") {
+      `Apache License
+      Version 2.0, January 2004
+      http://www.apache.org/licenses/`
+    }
+   }
 
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     return `# ${data.title}
 
-`;
+  `;
   }
 
   module.exports = generateMarkdown;
