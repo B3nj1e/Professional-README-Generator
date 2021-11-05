6a3bc222e097d0cde267ec64e6d7d0bb69cabfd4
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-05 10:31:41 +1030

    option list for licence question

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", "Please enter your GitHub username", "Please enter your email address", "Please enter a brief description of your project", "Please enter the installation instructions for this project", "Please enter usage instructions for this project", "Please enter the contribution guidelines", "Please enter the test instructions for this project", "Please choose a licence for your application from the list of options"];
+const questions = ["What is the title of your project?", "what is your GitHub username?", "What is your email address?", "Please enter a brief description of your project", "Please enter the installation instructions for this project", "Please enter usage instructions for this project", "Please enter the contribution guidelines", "Please enter the test instructions for this project", "Please choose a licence for your application from the list of options"];
 
 const names = ["title", "github", "email", "description", "installation", "usage", "contribution", "test", "licence"]
 
@@ -50,9 +50,10 @@ inquirer
         name: names[7],
     },
     {
-        type: 'input', 
+        type: 'list', 
         message: questions[8],
         name: names[8],
+        choices: ["no license", "open source", "MIT license", "private license"]
     },
 
     ])
